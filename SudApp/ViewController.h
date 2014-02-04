d99eaf8b8e82bb7b2879eac9c5075f1050b16e35
孝則 吉野 <takanoriyoshino@takanori-no-MacBook-Pro.local>
2014-02-04 09:51:28 +0800

    Initial Commit

---
+++ SudApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SudApp
+//
+//  Created by 孝則 吉野 on 2014/02/04.
+//  Copyright (c) 2014年 孝則 吉野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SudAppTests/SudAppTests.m
@@ -0,0 +1,34 @@
+//
+//  SudAppTests.m
+//  SudAppTests
+//
+//  Created by 孝則 吉野 on 2014/02/04.
+//  Copyright (c) 2014年 孝則 吉野. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SudAppTests : XCTestCase
+
+@end
+
+@implementation SudAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
